7e7aabac2758a62c079bfd5d917b9d4061015ff6
Crislany Victória <crislanyviictoria@gmail.com>
2025-08-10 08:13:33 -0300

    solução concluída do somando os valores da lista

---
+++ Somando_os_Valores_da_Lista.py
@@ -0,0 +1,7 @@
+#ID 8: Crie uma função que receba uma lista de números e retorne a soma de todos os seus valores.
+
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+def somar_lista(lista):
+    return sum(lista)
+print(somar_lista(numeros))
