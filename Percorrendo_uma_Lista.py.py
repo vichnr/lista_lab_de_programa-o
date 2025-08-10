c865c1802c67e92b81484240cd4b2894700341d9
Crislany Victória <crislanyviictoria@gmail.com>
2025-08-10 07:56:42 -0300

    solução concluída do Percorrendo uma Lista

---
+++ Percorrendo_uma_Lista.py.py
@@ -0,0 +1,10 @@
+#ID 1: Crie uma função que receba uma lista como argumento e percorra todos os seus elementos, imprimindo cada um na tela.
+
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+def receber_numeros(numeros):
+    for i in numeros:
+        print(i)
+    return
+
+receber_numeros(numeros)
